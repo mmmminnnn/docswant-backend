b2525804fa6c6fb362df7060c10112a32ca8af2a
mmmminnnn <rosofl28@naver.com>
2022-03-31 04:10:01 +0900

    project

---
+++ src/main/java/sju/capstone/docswant/DocswantApplication.java
@@ -0,0 +1,13 @@
+package sju.capstone.docswant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocswantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocswantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sju/capstone/docswant/DocswantApplicationTests.java
@@ -0,0 +1,13 @@
+package sju.capstone.docswant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocswantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
